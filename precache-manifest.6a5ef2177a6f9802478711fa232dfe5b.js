9753602d7dc9985b7a43915eea8036e3f273b230
StevenDixonDev <stevedixon1128.dev@gmail.com>
2019-10-29 11:55:26 -0400

    Updates

---
+++ precache-manifest.6a5ef2177a6f9802478711fa232dfe5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69c3aa8f800ebdfd55b1bbe67061ec2",
+    "url": "/Cardy_Clicky/index.html"
+  },
+  {
+    "revision": "8f52cae053756bb1d520",
+    "url": "/Cardy_Clicky/static/css/main.f03f5114.chunk.css"
+  },
+  {
+    "revision": "5b4c244e08958df141de",
+    "url": "/Cardy_Clicky/static/js/2.116045be.chunk.js"
+  },
+  {
+    "revision": "8f52cae053756bb1d520",
+    "url": "/Cardy_Clicky/static/js/main.a4e8775f.chunk.js"
+  },
+  {
+    "revision": "f6690b51850f5ac522ff",
+    "url": "/Cardy_Clicky/static/js/runtime-main.98cf11b6.js"
+  }
+]);
